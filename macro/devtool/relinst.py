a5ca4c85248e9de6ec4bac8e8fd57f5b8f9d0d12
Taewook Ko <427taewook@naver.com>
2026-04-28 22:19:21 +0900

    Fix path separator for Android Studio in relinst.py

--- macro/devtool/relinst.py
+++ macro/devtool/relinst.py
@@ -147,8 +147,8 @@ def newIconMac(name, exe, icon):
 """
 Android Signing Key (Using same PW with CERT_PW)
 
-cd "C:\Program Files\Android\Android Studio\jbr\bin"
-keytool -genkey -v -keystore "$HOME\Desktop\katusa.jks" -keyalg RSA -keysize 4096 -validity 10000 -alias k-atusa-soft
+cd "C:/Program Files/Android/Android Studio/jbr/bin"
+keytool -genkey -v -keystore "$HOME/Desktop/katusa.jks" -keyalg RSA -keysize 4096 -validity 10000 -alias k-atusa-soft
 """
 
 # get signtool from C:\Users\taewo\AppData\Local\Android\Sdk\build-tools\37.0.0\apksigner.bat
